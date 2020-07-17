a9eb06fec023f774a0d3d28bb9ea3cc83ba86d01
Umoren <samuelumoren365@gmail.com>
2020-07-17 12:07:19 +0100

    made fixes

--- capture.sql
+++ capture.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2020 at 09:05 AM
+-- Generation Time: Jul 17, 2020 at 01:02 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -11,6 +11,16 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `party-planner`
+--
 
 -- --------------------------------------------------------
 
@@ -22,6 +32,13 @@ CREATE TABLE `capture` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `capture`
+--
+
+INSERT INTO `capture` (`id`, `email`) VALUES
+(1, 'superadmin@classtivities.com')
 
 --
 -- Indexes for dumped tables
@@ -41,7 +58,7 @@ ALTER TABLE `capture`
 -- AUTO_INCREMENT for table `capture`
 --
 ALTER TABLE `capture`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
