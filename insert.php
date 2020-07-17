d081a0739897663827ac29ddb5f1e3cbb8bc338a
Umoren <samuelumoren365@gmail.com>
2020-07-17 10:34:04 +0100

    Fixed php code

--- insert.php
+++ insert.php
@@ -1,36 +1,36 @@
-<?php 
 
-$server = 'us-cdbr-east-02.cleardb.com';
-$username = 'ba3726e28e3e61';
-$password = 'a2195117';
-$database = 'heroku_5c84e53cd025961';
+<?php
+       $server = 'us-cdbr-east-02.cleardb.com';
+       $username = 'ba3726e28e3e61';
+       $password = 'a2195117';
+       $database = 'heroku_5c84e53cd025961';
 
-$connection = mysqli_connect($server, $username, $password);
-if (!$connection){
-    die("Database Connection Failed" . mysqli_error($connection));
-}
-$select_db = mysqli_select_db($connection, $database);
-if (!$select_db){
-    die("Database Selection Failed" . mysqli_error($connection));
-}
+        $connection = mysqli_connect($server, $username, $password, $database) or die(mysqli_error());
 
-if(isset($_POST) & !empty($_POST)){
- 
-   $email = $_POST['email'];
+    if(isset($_POST['submit'])) {
 
-   if (!isset($email) || empty($email)){
-        $error[] = "Email is required";
-    }
+        if (!empty($_POST['email'])){
+            $emailer = $_POST['email'];
+            $query = "SELECT * from emails where email='". $emailer . "'";
 
-    if(!isset($error) || empty($error)){
-       
-         $sql = "INSERT INTO `capture` (email) VALUES ('$email')";
-            if(mysqli_query($connection, $sql)){
-                header('Location: index.html');
-            } else{
-               echo 'query error: '. mysqli_error($connection);
+            $run = mysqli_query($connection,$query);
+
+            if(mysqli_num_rows($run) > 0){
+                http_response_code(403);
+                return ;
             }
-        
+
+            $query = "INSERT INTO emails (email) VALUES ('$emailer')";
+            $run = mysqli_query($connection,$query) or mysqli_error($connection);
+
+            if($run) {
+                echo "Great! You'd be the first to know when we launch!";
+            }else {
+                echo "There has been an error";
+            }
+        }else{
+            echo "All fields are required";
+        }
+
     }
-}
 ?>
